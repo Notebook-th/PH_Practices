0bef1b06659e81acc9eda8a55e14fe1fbc2d831a
Notebook-th <twakihasan@gmail.com>
2025-08-15 11:40:48 +0600

    first class of mod 15

--- Milestone 3/module 15/cl1.js
+++ Milestone 3/module 15/cl1.js
@@ -4,6 +4,16 @@
 
 const numbers = [1,2,3,4,5,6,7];
 
-console.log(numbers);
+// console.log(numbers);
+for (const num of numbers){
+    console.log(num);
+}
 
+const fruits = ['apple', 'mango', 'tomato', 'jackfruit'];
+
+for (const fruit of fruits){
+    console.log('i want to eat')
+    console.log(fruit)
+
+}
  
