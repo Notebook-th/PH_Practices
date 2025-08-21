36f76bf7a86f6842e43c847131d36d133947e003
Notebook-th <twakihasan@gmail.com>
2025-08-21 20:47:52 +0600

    module 25 class 3

--- milestone 5/module 25/java/events.js
+++ milestone 5/module 25/java/events.js
@@ -1,1 +1,27 @@
 console.log('external file')
+
+ function makeYellow(){
+            document.body.style.backgroundColor = 'yellow'
+        }
+        function makeWhite(){
+            document.body.style.backgroundColor = 'blue'
+        }
+        function makeRed(){
+            document.body.style.backgroundColor = 'Red'
+        }
+
+        const btnMakeViolate = document.getElementById('btn-make-violate')
+        // console.log(btnMakeViolate)
+        btnMakeViolate.onmouseover = function makeblue(){
+            document.body.style.backgroundColor = 'gold'
+        }
+
+        const btnMakePurple = document.getElementById ('btn-make-purple')
+        btnMakePurple.onclick = makepurple;
+            function makepurple(){
+            document.body.style.backgroundColor = 'purple'
+            }
+
+            document.getElementById('btn-make-green').addEventListener('click', function (){
+            document.body.style.backgroundColor = 'green'
+            })
