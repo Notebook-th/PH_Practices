d8761bb2af2de9fbbe3bd58fce97ef73f86e43d5
Notebook-th <twakihasan@gmail.com>
2025-08-22 12:02:43 +0600

    module 25 class 3 updated

--- milestone 5/module 25/java/handler.js
+++ milestone 5/module 25/java/handler.js
@@ -1,1 +1,7 @@
 console.log('line lagse')
+
+document.getElementById('btn-update-title')
+.addEventListener('click', function(){
+    const titleText = document.getElementById('title-text')
+    titleText.innerText = 'hi my name is twaki' 
+})
