7da34b572df7980abe54344ae8f385c60f5b2a87
Notebook-th <twakihasan@gmail.com>
2025-08-30 20:04:37 +0600

    milestone 6

---
+++ Milestone 6/Module 29/let-const.js
@@ -0,0 +1,12 @@
+const name = 'Bangladesh' ;
+const country = 'Name is :' + name;
+console.log(country)
+
+const dress = ['jama', 'kapor', 'pant' ]
+// dress = [] ;
+dress.push ('mafler')
+console.log(dress)
+
+const stu = {names: 'hasan', marks: 45 }
+stu.marks = 70;
+console.log(stu)
---
+++ Milestone 6/Module 29/parameter.js
@@ -0,0 +1,5 @@
+function sum(n1, n2){
+    const total = n1 + n2;
+    console.log(total);
+}
+sum(10,20)
