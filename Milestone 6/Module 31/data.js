7729bb65d2930a42daf826438a11534b3ddf5e67
Notebook-th <twakihasan@gmail.com>
2025-09-06 10:52:19 +0600

    module 31 started

---
+++ Milestone 6/Module 31/data.js
@@ -0,0 +1,17 @@
+// js is dynamic typed programming lang
+
+let data = 4;
+data = 'twaki';
+data = false;
+data = {name: 'twaki', price: 50};
+console.log(typeof data)
+
+// premitive
+const price = 40;
+const namre = "twaki"
+const isPoor = false;
+let isKnown = undefined;//should not be assigned by the user
+
+// non-premitive
+const student = { name : 'twski', age: 23}
+const friednds = ["abul", "kabul"];
